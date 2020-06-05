76f02bd5705241e271902d2ad54f9bbe3fa1a27d
Alin Balasa <balasaalin@gmail.com>
2020-06-05 16:46:56 +0300

    Download OPE

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -2,17 +2,21 @@
 
 rm -rf output
 
-REPONAME=`basename $PWD`
-PARENTDIR=`dirname $PWD`
-USERNAME=`basename $PARENTDIR`
+echo "Download Oxygen Publishing Engine"
+wget https://mirror.oxygenxml.com/InstData/PublishingEngine/oxygen-publishing-engine-3.x.zip
+unzip oxygen-publishing-engine-3.x.zip
+
+echo "Inject license"
 
 # Writes the license key in a file
 # Assumes that $LICENSE_KEY contains the license key with all /n's replaced by @@
-printf "%s" "$LICENSE_KEY" > tools/oxygen-publishing-engine-3.x/licensekey.txt
-sed -i 's/@@/\n/g' tools/oxygen-publishing-engine-3.x/licensekey.txt
+printf "%s" "$LICENSE_KEY" > oxygen-publishing-engine-3.x/licensekey.txt
+sed -i 's/@@/\n/g' oxygen-publishing-engine-3.x/licensekey.txt
 
 
 # Send some parameters to the "editlink" plugin as system properties
 export ANT_OPTS="$ANT_OPTS"
 
-tools/oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -o $OUT_DIR -t $TEMP_DIR -Dwebhelp.publishing.template.descriptor=$OPT_FILE -Dwebhelp.publishing.template=$OPT_DIR
+echo "Generate WebHelp Responsive Output from " ${DITAMAP}
+
+oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -o $OUT_DIR -t $TEMP_DIR -Dwebhelp.publishing.template.descriptor=$OPT_FILE -Dwebhelp.publishing.template=$OPT_DIR
