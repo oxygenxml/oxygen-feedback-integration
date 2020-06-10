b0df276b46e1c9b3c56b945a137b055b79485ca3
Alin Balasa <balasaalin@gmail.com>
2020-06-10 18:29:48 +0300

    + ditaval file

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -14,4 +14,4 @@ cp licenseKey.txt oxygen-publishing-engine-3.x/licensekey.txt
 
 echo "Generate WebHelp Responsive Output from " ${DITAMAP}
 
-oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -o $OUT_DIR -t $TEMP_DIR -Dwebhelp.publishing.template.descriptor=$OPT_FILE -Dwebhelp.publishing.template=$OPT_DIR
+oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -filter=$DITAVAL -o $OUT_DIR -t $TEMP_DIR -Dwebhelp.publishing.template.descriptor=$OPT_FILE -Dwebhelp.publishing.template=$OPT_DIR
